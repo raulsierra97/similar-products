fc46506fc59071cdb124289de1ff38e4e9097b0b
raulsierra97 <raulsiemar@gmail.com>
2025-04-01 17:56:13 +0200

    feat: Initialize Spring Boot project with basic structure

---
+++ src/test/java/com/raulsierra/similarproducts/SimilarProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raulsierra.similarproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
